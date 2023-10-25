61c5c10e68ec74394d6cfdeca411dda373c836a8
lazcanoluca <lazcanoluca@gmail.com>
2023-10-24 23:46:52 -0300

    refactor: borron y cuenta nueva, tabula rasa, barajo de nuevo

--- PostComponent.js
+++
@@ -1,9 +0,0 @@
-class PostComponent extends HTMLElement {
-    constructor() {
-        super();
-        this.innerHTML = `<h1>Hi, soy custom.</h1>`;
-    }
-}
-
-window.customElements.define('post-component', PostComponent);
-
--- userCard.js
+++
@@ -1,8 +0,0 @@
-class UserCard extends HTMLElement {
-    constructor() {
-        super();
-        this.innerHTML = `John Doe`;
-    }
-}
-
-window.customElements.define('user-card', UserCard);
