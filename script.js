4204cd3a3260fd2153f4d0e1f07578853faf813a
lazcanoluca <lazcanoluca@gmail.com>
2023-10-24 23:47:22 -0300

    feat: carga un post con la metadata, en base a la url, luca happy

---
+++ script.js
@@ -0,0 +1,57 @@
+// script.js
+// Include the common header and footer
+// fetch('header.html')
+//     .then(response => response.text())
+//     .then(data => {
+//         document.getElementById('header-placeholder').innerHTML = data;
+//     });
+
+// fetch('footer.html')
+//     .then(response => response.text())
+//     .then(data => {
+//         document.getElementById('footer-placeholder').innerHTML = data;
+//     });
+
+// Function to load content based on the hash
+function loadContent() {
+    const hash = window.location.hash.substr(1); // Get the hash part without the #
+    let contentFile = 'pages/home.html'; // Default content
+
+    if (hash === 'about') {
+        contentFile = 'pages/about.html';
+    } else if (hash === 'posts') {
+        contentFile = 'pages/posts.html'
+    } else if (hash === 'a_post') {
+        let postLayoutFile = 'pages/post.html'
+
+        fetch(postLayoutFile)
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('content').innerHTML = data;
+            });
+
+        fetch('posts/post_example.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('post-content').innerHTML = data;
+            })
+            .then(() => {
+                let meta = document.getElementsByTagName('meta');
+                let autor = meta.namedItem('autor').content;
+                document.getElementById('author').innerHTML = `<p>${autor}</p>`;
+            })
+    }
+
+    // fetch(contentFile)
+    //     .then(response => response.text())
+    //     .then(data => {
+    //         document.getElementById('content').innerHTML = data;
+    //     });
+
+    console.log(hash)
+}
+
+window.addEventListener('hashchange', loadContent);
+
+// Initial page load
+loadContent();
