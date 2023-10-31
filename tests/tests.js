d9da3a38349b8cbd9cc6710eb3eb0f81d8b6d05c
lazcanoluca <lazcanoluca@gmail.com>
2023-10-31 00:29:35 -0300

    tests posts

--- tests/tests.js
+++ tests/tests.js
@@ -1,19 +1,23 @@
 import { loadPosts, parseHtmlToPost } from '../js/posts.js';
 
-describe('Posts', () => {
-    it('works', async () => {
-        await expectAsync(loadPosts()).toBeResolvedTo([])
-    })
-})
+import { test, assertEqual } from '../lib/microtesting/index.js'
+
+// import { describe, it } from '../lib/jasmine/jasmine.js'
+
+// describe('Posts', () => {
+//     it('works', async () => {
+//         await expectAsync(loadPosts()).toBeResolvedTo([])
+//     })
+// })
 
 // console.log('Empezando tests...')
 
-// test('loadPosts', () => {
-//     return loadPosts('./posts_test')
-//         .finally(posts => {
-//             assertEqual(posts, ['hola']);
-//         })
-// });
+test('loadPosts', async () => {
+    await assertEqual(
+        await loadPosts('../tests/posts_test'),
+        ['/tests/posts_test/another_post_example.html', '/tests/posts_test/post_example.html']
+    )
+});
 
 // test('createPublicationYearLabel: a single year', () => {
 //     assertEqual(1, 1);
