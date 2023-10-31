d581229832ba6fb03026bddc06605b8a1093f0cf
lazcanoluca <lazcanoluca@gmail.com>
2023-10-31 13:59:13 -0300

    add and better tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,10 +1,26 @@
-import { loadPosts, parseHtmlToPost } from '../js/posts.js';
-import { test, assertEqual } from '../lib/microtesting/index.js'
+import { loadPosts, getPostMetadata } from '../js/posts.js';
+import { test, assertEqual, assertEqualArrays, assertEqualObjects } from '../lib/microtesting/index.js'
 
-test('loadPosts', async () => {
-    await assertEqual(
-        await loadPosts('../tests/posts_test'),
-        ['/tests/posts_test/another_post_example.html', '/tests/posts_test/post_example.html']
-    )
-});
+test('loadPosts: debería obtener las rutas de los posts en la carpeta data',
+    async () => {
+        await assertEqual(
+            await loadPosts('../tests/posts_test'),
+            [
+                '/tests/posts_test/another_post_example.html',
+                '/tests/posts_test/post_example.html'
+            ]
+        )
+    }
+);
 
+test('getPostMetadata: debería devolver la metadata dada la ruta a un post válido',
+    async () => {
+        await assertEqualObjects(
+            await getPostMetadata('/tests/posts_test/post_example.html'),
+            {
+                autor: 'Luca',
+                titulo: 'Prueba'
+            }
+        )
+    }
+);
