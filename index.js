84fdf0532eae2df73bc34b3bd2b42c2cb2e6f22e
lazcanoluca <lazcanoluca@gmail.com>
2023-11-02 20:25:44 -0300

    routing y algo asi

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// import { loadPosts } from "./js/posts.js";
+// import { getPostMetadata } from "./js/posts"
 
 /**
  * 
@@ -15,7 +15,21 @@ function setSelectedNavItem(hash) {
     document.getElementById(hash)?.classList.add('selected')
 }
 
-function loadPage() {
+async function loadPost(postHash) {
+    const postRoute = `./posts/${postHash}`
+
+    try {
+        const metadata = await getPostMetadata(postRoute)
+        const layout = await fetch(postRoute).text()
+        document.getElementById('content-placeholder').innerHTML = layout;
+        document.getElementById('post-title').innerHTML = metadata.titulo;
+        document.getElementById('post-author').innerHTML = metadata.autor;
+    } catch (err) {
+        throw new Error(err)
+    }
+}
+
+async function loadPage() {
     const hash = window.location.hash.substr(1); // Get the hash part without the #
     let contentFile = 'pages/home.html'; // Default content
     const postHash = hash.startsWith('post/') ? hash.split('/')[1] : null
@@ -24,47 +38,24 @@ function loadPage() {
     // Cada página se llama como su ruta. Corta.
     contentFile = hash ? `pages/${hash}.html` : 'pages/home.html'
 
-    // if (hash === 'about') {
-    //     contentFile = 'pages/about.html';
-    // } else if (hash === 'posts') {
-    //     contentFile = 'pages/posts.html'
-    //     // loadPosts()
-    // } else if (hash === 'a_post') {
-    //     let postLayoutFile = 'pages/post.html'
+    if (postHash) {
+        try {
+            await loadPost(postHash)
+        } catch {
+            document.getElementById('content-placeholder').innerHTML = `<p>Error al cargar el post.</p>`;
+        }
+    } else {
+        fetch(contentFile)
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('content-placeholder').innerHTML = data;
+                setSelectedNavItem(`${hash || 'home'}`)
+            })
+    }
 
-    //     fetch('./layouts.post')
-    //         .then(response => response.text())
-    //         .then(data => {
-    //             document.getElementById('content').innerHTML = data;
-    //         });
-
-    //     fetch('posts/post_example.html')
-    //         .then(response => response.text())
-    //         .then(data => {
-    //             document.getElementById('post-content').innerHTML = data;
-    //         })
-    //         .then(() => {
-    //             let meta = document.getElementsByTagName('meta');
-    //             let autor = meta.namedItem('autor').content;
-    //             document.getElementById('author').innerHTML = `<p>${autor}</p>`;
-    //         })
-    // }
-
-    // if (postHash) {
-    //     try {
-    //         const layout = await fetch(`./posts/${postHash}`).text()   
-    //     }
-    // }
-
-    fetch(contentFile)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('content-placeholder').innerHTML = data;
-            setSelectedNavItem(`${hash || 'home'}`)
-        })
-        .catch(
-            document.getElementById('content-placeholder').innerHTML = `<h2>Oops...</h2>`
-        )
+    // .catch(
+    //     document.getElementById('content-placeholder').innerHTML = `<h2>Oops...</h2>`
+    // )
 
     console.log(hash)
 }
@@ -72,4 +63,4 @@ function loadPage() {
 window.addEventListener('hashchange', loadPage);
 
 // Initial page load
-loadPage();
+await loadPage();
